ca75aa98dff1c29f7906c840f489643c27495ed8
Annah <annah.gerletti@fluxfederation.com>
2018-05-01 10:03:23 +1200

    built a little program that has several user imput and the program takes them in while outputting

--- play_ground.rb
+++ play_ground.rb
@@ -1,12 +1,14 @@
+
 #having issues with this is codeacademy, the chainning was not working.
 # ! bang 
 # bang methods work on strings but return nil 
 # bang has to be explicitly defined 
 #did not work bc method with bang dod not return the string 
-# 
+# Rmoved the ! bang MADE IT WORK
 
 print "What's your first name?"
 first_name = gets.chomp.capitalize
+#befor - first_name = gets.chomp.capitalize! (didn't work)
 
 
 print "What is your family name?"
@@ -25,3 +27,14 @@ puts "Is that in #{state}?"
 
 puts "my name is #{first_name} #{last_name} and I live in #{city} which is in #{state}"
 
+#good practice for Hang man 
+print "what is your name?"
+name = gets.chomp.capitalize
+
+print "How old are you?"
+require "byebug" #keep in seperate lines 
+byebug
+age = gets.chomp
+
+
+print "Hi, I am #{name} and I am #{age} years old" 
