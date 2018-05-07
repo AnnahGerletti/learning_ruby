32efb0f08891f230259e3debd833e5ff2ed3a44b
Annah <annah.gerletti@fluxfederation.com>
2018-05-07 14:17:11 +1200

    need to understand funtions for hangman

---
+++ functions.rb
@@ -0,0 +1,20 @@
+#Functions in Ruby also called Methods
+# Code scripts can take ARGUMENTS known as ARGV input_file = ARGV.first
+#This is simmilar to giving a function atributes 
+
+
+def one_plus_one (num)
+    new = num + 3
+    print new 
+    
+end
+
+one_plus_one(3)
+
+def christmas (gifts, num)
+    puts "\n""I got #{num} #{gifts}"
+end
+
+christmas( 'trucks',  2+2)
+
+
