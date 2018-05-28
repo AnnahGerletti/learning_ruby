499ba222b832cbfc6abba6499a182dcb9d356fbc
Annah <annah.gerletti@fluxfederation.com>
2018-05-29 09:58:05 +1200

    more debuging with byebug

--- bye_bug.rb
+++ bye_bug.rb
@@ -1,23 +1,33 @@
 
     # Byebug - is the debuging software that FLUX uses 
     # It had to be installed and you must require it into the program, then you have set a break pt or byebug 
+   
+    require "byebug"
+
     # they have to be on seperate line the byebug and the require
     # U then can interact with it inside the terminal 
-    # if you need to jump down some lines use the next cmd
+    
+    #  to jump down some lines use the 
+       next 
 
-print "What do you want" 
+    # to continue through the code 
+        c
+    
+    # can type any var or a line of code and see what the output is
+        cup = [3, 4]
 
-require "byebug"
+        cup # just tells you what the imput was
+        cup.length # give you the length of the arr
+        cup.class # gives you the data type of cup
 
-byebug
-answer = gets.chomp
 
-print "I really want a #{answer}"
+        # example how to formulate byebug
+        print "What do you want" 
+        byebug
+        answer = gets.chomp
+
+        print "I really want a #{answer}"
 
 
     # When you require byebug delete the code after finished
-    # the require and byebug call have be seperated either by ; or separet lines
-    # next - will jump to the next lines in the code 
-    # <variable name>.class - tell you the data type
-    # <variable name> - tell you what the imput was 
     # to exit byebug type 'exit'
